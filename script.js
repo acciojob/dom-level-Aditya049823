92b6c41d24160366446c3b47e70f19b526e8fa0c
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-07-25 09:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const element=document.getElementById("level");
+let n=Number.MIN_VALUE;
+if(element.style!==undefined){
+	n=0;
+}
+else if(element.getAttribute!==undefined){
+	n=1;
+}
+else if(element.addEventListener!==undefined){
+	n=2;
+}
+else{
+	n=3;
+}
+alert("The level of the element is: "+n);
